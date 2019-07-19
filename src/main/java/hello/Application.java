dfd548b9e1d9d842b244594401b39f70daf77a4f
cherhio <cherhio@163.com>
2019-07-19 14:40:35 +0800

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cherhio
+ * @date Created in 2019-07-17 11:58
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * @author cherhio
+ * @date Created in 2019-07-17 11:49
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
